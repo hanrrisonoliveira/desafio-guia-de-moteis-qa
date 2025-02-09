bdbd8da1d9e61ddce9e25afb84fed979ec33fa00
Hanrrison Oliveira <hanrrison.dos@gmail.com>
2025-02-09 18:27:53 -0300

    test: Criação dos casos de teste

--- Testes de API - (Jest)/__tests__/apimock.spec.js
+++ Testes de API - (Jest)/__tests__/apimock.spec.js
@@ -1,7 +1,34 @@
 const { API_URL } = require('../config')
 const request = require('supertest')
 
-it('Primeiro teste', async () => {
-    const response = await request(API_URL).get('/users')
-    console.log(response.body)
+describe('Testes do tipo /GET', () => {
+    it('Validar a lista de usuários com status 200', async () => {
+        const response = await request(API_URL).get('/users')
+       
+        expect(response.status).toBe(200)
+        
+        // Valido se é um array e que não está vazio
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBeGreaterThan(0)
+        
+        // Valido a estrutura do array
+        response.body.forEach(user => {
+            expect(user).toHaveProperty('id')
+            expect(user).toHaveProperty('name')
+            expect(user).toHaveProperty('username')
+            expect(user).toHaveProperty('email')
+            expect(user).toHaveProperty('address')
+            expect(user).toHaveProperty('phone')
+            expect(user).toHaveProperty('website')        
+        })
+    })
+})
+
+describe ('Testes do tipo /POST', () => {
+    it('Deve retornar o status 404 em um endpoint inválido', async () => {
+        const response = await request(API_URL).get('/postssssss')
+
+        expect(response.status).toBe(404)
+        expect(response.body).toEqual({})
+    })
 })
